7c7f64cae06e8f9250c971d4f4764819ee125d03
abdoulakeem998 <abdoul.akeem998@gmail.com>
2025-12-11 02:53:08 +0000

    done

--- includes/header.php
+++ includes/header.php
@@ -1,11 +1,14 @@
 <?php
 $current_page = basename($_SERVER['PHP_SELF']);
+
+// Get base path for links (defined in config/database.php)
+$base = defined('BASE_PATH') ? BASE_PATH : '/';
 ?>
 <header class="main-header">
     <nav class="navbar navbar-expand-lg">
         <div class="container">
-            <a class="navbar-brand" href="/AR-Rhma-final/index.php">
-                <img src="/AR-Rhma-final/assets/images/logo.png" alt="AR-Rahma" style="height: 60px; width: auto;">
+            <a class="navbar-brand" href="<?php echo $base; ?>index.php">
+                <img src="<?php echo $base; ?>assets/images/logo.png" alt="AR-Rahma" style="height: 60px; width: auto;">
             </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
                 <span class="navbar-toggler-icon"></span>
@@ -14,31 +17,31 @@ $current_page = basename($_SERVER['PHP_SELF']);
                 <ul class="navbar-nav ms-auto">
                     <li class="nav-item">
                         <a class="nav-link <?php echo ($current_page == 'index.php') ? 'active' : ''; ?>" 
-                           href="/AR-Rhma-final/index.php">Home</a>
+                           href="<?php echo $base; ?>index.php">Home</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link <?php echo ($current_page == 'about.php') ? 'active' : ''; ?>" 
-                           href="/AR-Rhma-final/about.php">About Us</a>
+                           href="<?php echo $base; ?>about.php">About Us</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link <?php echo ($current_page == 'activities.php') ? 'active' : ''; ?>" 
-                           href="/AR-Rhma-final/activities.php">Activities</a>
+                           href="<?php echo $base; ?>activities.php">Activities</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link <?php echo ($current_page == 'team.php') ? 'active' : ''; ?>" 
-                           href="/AR-Rhma-final/team.php">Our Team</a>
+                           href="<?php echo $base; ?>team.php">Our Team</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link <?php echo ($current_page == 'volunteer.php') ? 'active' : ''; ?>" 
-                           href="/AR-Rhma-final/volunteer.php">Volunteer</a>
+                           href="<?php echo $base; ?>volunteer.php">Volunteer</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link <?php echo ($current_page == 'gallery.php') ? 'active' : ''; ?>" 
-                           href="/AR-Rhma-final/gallery.php">Gallery</a>
+                           href="<?php echo $base; ?>gallery.php">Gallery</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link <?php echo ($current_page == 'contact.php') ? 'active' : ''; ?>" 
-                           href="/AR-Rhma-final/contact.php">Contact</a>
+                           href="<?php echo $base; ?>contact.php">Contact</a>
                     </li>
                     
                     <?php if (isLoggedIn()): ?>
@@ -47,25 +50,25 @@ $current_page = basename($_SERVER['PHP_SELF']);
                             <i class="bi bi-person-circle"></i> My Account
                         </a>
                         <ul class="dropdown-menu dropdown-menu-end">
-                            <li><a class="dropdown-item" href="/AR-Rhma-final/my_applications.php">My Applications</a></li>
-                            <li><a class="dropdown-item" href="/AR-Rhma-final/profile.php">Profile</a></li>
+                            <li><a class="dropdown-item" href="<?php echo $base; ?>my_applications.php">My Applications</a></li>
+                            <li><a class="dropdown-item" href="<?php echo $base; ?>profile.php">Profile</a></li>
                             <li><hr class="dropdown-divider"></li>
-                            <li><a class="dropdown-item" href="/AR-Rhma-final/logout.php">Logout</a></li>
+                            <li><a class="dropdown-item" href="<?php echo $base; ?>logout.php">Logout</a></li>
                         </ul>
                     </li>
                     <?php else: ?>
                     <li class="nav-item">
-                        <a class="nav-link" href="/AR-Rhma-final/login.php">
+                        <a class="nav-link" href="<?php echo $base; ?>login.php">
                             <i class="bi bi-box-arrow-in-right"></i> Login
                         </a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="/AR-Rhma-final/signup.php">Sign Up</a>
+                        <a class="nav-link" href="<?php echo $base; ?>signup.php">Sign Up</a>
                     </li>
                     <?php endif; ?>
                     
                     <li class="nav-item">
-                        <a class="btn btn-donate" href="/AR-Rhma-final/donate.php">
+                        <a class="btn btn-donate" href="<?php echo $base; ?>donate.php">
                             <i class="bi bi-heart"></i> Donate
                         </a>
                     </li>
