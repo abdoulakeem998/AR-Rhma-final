cc841bff3f7bb3bbadeacedb2f55c3386eae6e1e
abdoulakeem998 <abdoul.akeem998@gmail.com>
2025-12-25 21:49:28 +0000

    done

--- admin/includes/admin_header.php
+++ admin/includes/admin_header.php
@@ -8,7 +8,7 @@ requireAdmin();
     <title>Admin Panel - AR-Rahma</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.0/font/bootstrap-icons.css">
-    <link rel="stylesheet" href="../assets/css/style.css">
+    <link rel="stylesheet" href="../css/style.css">
 </head>
 <body>
 <div class="container-fluid">
